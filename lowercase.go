7951c31c9308da9c53350b99da90487c9f1ed5f7
ozkar <oscarmg99@gmail.com>
2020-02-22 19:29:23 -0800

    fixes typo

--- lowercase.go
+++ lowercase.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-/*Lowercase makes all of the routes lowercase internally, /route != /Route in go (or the html standard) but in most languages they are */
+/*Lowercase makes all of the routes lowercase internally, /route != /Route in go (or the http standard) but in most languages they are */
 func Lowercase(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.URL.Path = strings.ToLower(r.URL.Path)
